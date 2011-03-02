3440088a4db8815fd4b05e8f2b5c4f46ff0cebd3
GyD <japanimcity@gmail.com>
2011-03-02 15:59:30 +0100

    manquais des fichiers

---
+++ src/GyD/EverShop/EverShopPlayerListener.java
@@ -0,0 +1,23 @@
+package GyD.EverShop;
+
+import org.bukkit.Location;
+import org.bukkit.entity.Player;
+import org.bukkit.event.player.PlayerChatEvent;
+import org.bukkit.event.player.PlayerEvent;
+import org.bukkit.event.player.PlayerListener;
+import org.bukkit.event.player.PlayerMoveEvent;
+
+/**
+ * Handle events for all Player related events
+ * @author GyD
+ */
+public class EverShopPlayerListener extends PlayerListener {
+    private final EverShop plugin;
+
+    public EverShopPlayerListener(EverShop instance) {
+        plugin = instance;
+    }
+
+    //Insert Player related code here
+}
+
